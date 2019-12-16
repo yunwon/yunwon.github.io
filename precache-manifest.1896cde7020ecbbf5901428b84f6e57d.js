7a233b5309dcd6d679d287625ea0d21915e595a0
jeongyun <jeongyunw@gmail.com>
2019-12-17 10:29:54 +1300

    deploying

---
+++ precache-manifest.1896cde7020ecbbf5901428b84f6e57d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b43d1f2302e5dcbfeffdc7c1321eb37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "357889b2d6d42328f5f5",
+    "url": "/static/css/main.b7c434f1.chunk.css"
+  },
+  {
+    "revision": "ea752e60bb2017456d72",
+    "url": "/static/js/2.867f13b6.chunk.js"
+  },
+  {
+    "revision": "357889b2d6d42328f5f5",
+    "url": "/static/js/main.5b8504f9.chunk.js"
+  },
+  {
+    "revision": "a00c1c5cf5a278e86527",
+    "url": "/static/js/runtime-main.22fc3320.js"
+  },
+  {
+    "revision": "c5aa50ff7cade43a49eec72d19c277e7",
+    "url": "/static/media/JeongyunWon_CV.c5aa50ff.pdf"
+  },
+  {
+    "revision": "5bea483d149f3738d424e8fd285868e2",
+    "url": "/static/media/detail.5bea483d.png"
+  },
+  {
+    "revision": "c585a7bfcd26f34fafcee5e77e7407dc",
+    "url": "/static/media/easyforms-main.c585a7bf.png"
+  },
+  {
+    "revision": "bdc9ea0028cb69255792e06f09200580",
+    "url": "/static/media/easyforms-video.bdc9ea00.mp4"
+  },
+  {
+    "revision": "438506821ffa784a5640eb3cb5471d93",
+    "url": "/static/media/easyforms.43850682.png"
+  },
+  {
+    "revision": "72fdec3385dd0e123716c49ee0df7f79",
+    "url": "/static/media/jeongyunwon.72fdec33.png"
+  },
+  {
+    "revision": "533717116bcccaa0baf9080e39649ce8",
+    "url": "/static/media/mobile-all.53371711.png"
+  },
+  {
+    "revision": "2c9b207564a3a4fdb6f4d5363c3f0676",
+    "url": "/static/media/people-screens.2c9b2075.png"
+  },
+  {
+    "revision": "239932082a8f295ffb6fc191982f287d",
+    "url": "/static/media/responsive.23993208.png"
+  },
+  {
+    "revision": "1da5cb66012e95c1ca836374ecef9bc6",
+    "url": "/static/media/toolbox-video.1da5cb66.mp4"
+  },
+  {
+    "revision": "15de5bcf42fdfbaecbf7c3eb49cc0aae",
+    "url": "/static/media/toolbox.15de5bcf.png"
+  },
+  {
+    "revision": "9d3db825a946de7fe1b02302512176d0",
+    "url": "/static/media/toolbox.9d3db825.png"
+  },
+  {
+    "revision": "f2ed90aa9d41913110a7e3f1e0dc1716",
+    "url": "/static/media/wcb-main.f2ed90aa.png"
+  },
+  {
+    "revision": "94ec46fa93512cf0fa9d2647fe3f5edc",
+    "url": "/static/media/wcb-thumb.94ec46fa.png"
+  }
+]);
