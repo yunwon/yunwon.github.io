8940d409e72ab7f6779e9d393d508a5d4b402805
yunwon <jeongyunw@gmail.com>
2025-02-11 18:02:24 +0900

    Updated About page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Fade from "react-reveal/Fade";
 import Me from "../../assets/about/jeongyunwon.png";
-import CV from "../../assets/about/JeongyunWon_CV.pdf";
+import CV from "../../assets/about/yunwon_cv.pdf";
 import styles from "./About.module.scss";
 import { Layout } from "../../components/Layout/Layout";
 
@@ -48,23 +48,24 @@ export default class About extends Component {
                 style={{ marginTop: "0.5rem" }}
               >
                 <div className={styles.contents_left}>
-                  <h4>Graphic/Web design</h4>
+                  <h5>Design</h5>
                   <div className={styles.description}>
                     <ul>
-                      <li>Graphic Design</li>
-                      <li>UI design</li>
-                      <li>UX design</li>
+                      <li>UX/UI design</li>
+                      <li>Graphic design (Digital/Print)</li>
+                      <li>Animation</li>
                     </ul>
                   </div>
                 </div>
                 <div className={styles.contents_right}>
-                  <h4>Front-end Develepment</h4>
+                  <h5>Web Develepment</h5>
                   <div className={styles.description}>
                     <ul>
                       <li>HTML5, CSS3 (SASS/SCSS)</li>
                       <li>Responsive Design</li>
-                      <li>Javascript (React JS / Next JS / TypeScript / jQuery)</li>
+                      <li>Javascript (React JS, jQuery)</li>
                       <li>Bootstrap, Tailwind CSS</li>
+                      <li>HTML email</li>
                       <li>
                         WordPress <br />
                         (Have experiences in Divi / WooCommerce / Elementor)
@@ -77,8 +78,10 @@ export default class About extends Component {
             <this.renderSkills title={`Softwares that \nI use are`}>
               <div className={styles.description}>
                 <ul>
-                  <li>Adobe XD, Illustrator, Photoshop, Indesign, After Effect</li>
                   <li>Figma</li>
+                  <li>Adobe Illustrator, Photoshop, Indesign, After Effect, XD</li>
+                  <li>Google Analytics, Google Tag Manager</li>
+                  <li>Webflow</li>
                   <li>Github / VSCode</li>
                 </ul>
               </div>
@@ -87,13 +90,13 @@ export default class About extends Component {
               <div className={styles.description}>
                 <div style={{ marginBottom: "2.5rem" }}>
                   <p className={styles.year}>
-                    <strong>May 2021 - present</strong>
+                    <strong>May 2021 - Dec 2024</strong>
                   </p>
                   <p>
                     <b>Ingram Micro (Agency Ingram Micro)</b>
                     <br />
-                    Graphic designer<br/>
-                    <small><i>Creating artwork for digital campaigns. UX/UI design and building front-end for EDM and websites</i></small>
+                    as a Graphic/Web designer<br />
+                    <small><i>Creating artwork for digital campaigns. UX/UI design and building front-end for HTML emails and websites</i></small>
                   </p>
                 </div>
                 <div style={{ marginBottom: "2.5rem" }}>
@@ -103,7 +106,7 @@ export default class About extends Component {
                   <p>
                     <b>Freelancing</b>
                     <br />
-                    as a graphic/UX/UI designer and front-end developer<br/>
+                    as a graphic/UX/UI designer and front-end developer<br />
                     <small><i>Main Client: Bouqo, History of Egypt Podcast</i></small>
                   </p>
                 </div>
